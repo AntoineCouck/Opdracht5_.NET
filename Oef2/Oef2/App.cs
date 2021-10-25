42ecb4263bbae5ba0e9652170ba61a17caca0000
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-10-25 22:00:43 +0200

    Add files via upload
    Oefeningen 1 tot 3 van opdracht 5

---
+++ Oef2/Oef2/App.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Oef2
+{
+    public partial class Gemiddelde : Form
+    {
+
+
+
+        public Gemiddelde()
+        {
+            InitializeComponent();
+        }
+
+        private void NieuwGetalButton_Click(object sender, EventArgs e)
+        {
+
+            UserDialog dialogWindow = new UserDialog();
+            dialogWindow.ShowDialog();
+
+            if (dialogWindow.DialogResult == DialogResult.OK)
+            {
+                AddNumber(dialogWindow.number);
+                CalculateAVG();
+                dialogWindow.Dispose();
+            }
+        }
+
+
+        public void AddNumber(double number)
+        {
+            GetallenLijst.Items.Add(number);
+
+        }
+
+
+        public void CalculateAVG()
+        {
+
+            double total = 0;
+
+
+            for (int i = 0; i < GetallenLijst.Items.Count; i++)
+            {
+                total = total + double.Parse(GetallenLijst.Items[i].ToString());
+            }
+
+            total = (total / GetallenLijst.Items.Count);
+
+
+            gem.Text = total.ToString();
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Oef3/Oef3/ColorDialog.cs
@@ -0,0 +1,10 @@
+﻿namespace Oef3
+{
+    public partial class ColorDialog : Form
+    {
+        public ColorDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
